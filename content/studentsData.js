336ae86e01dbeff4f9b6a1fe28176121b03a9406
tangerinka410 <46780519+tangerinka410@users.noreply.github.com>
2019-01-17 11:41:21 +0100

    Update studentsData.js

--- content/studentsData.js
+++ content/studentsData.js
@@ -5,7 +5,8 @@ const studentsData = {
   ],
   mgrStudents: [
     { name: "Bc. Student", link: "https://is.muni.cz" },
-    { name: "Bc. Student2", link: "https://is.muni.cz" }
+    { name: "Bc. Student2", link: "https://is.muni.cz" },
+    { name: "Bc. Student3", link: "https://is.muni.cz" },
   ],
   bcStudents: [
     { name: "Student", link: "https://is.muni.cz" },
